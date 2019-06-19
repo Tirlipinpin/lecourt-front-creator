104fc93a4c05fa3658fee8c686a7f4920edfd3be
Tirlipinpin <hgaudeaux@hotmail.fr>
2019-06-19 11:57:19 +0200

    :white_check_mark: added tests on UploadMovie component

--- src/components/App/UploadMovie/index.test.tsx
+++ src/components/App/UploadMovie/index.test.tsx
@@ -1,18 +1,22 @@
-import React, { SyntheticEvent } from 'react';
+import React, {FormEvent, SyntheticEvent} from 'react';
 import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
-
-import UploadMovie from '.';
 import { UploadChangeParam } from 'antd/lib/upload';
+import { UploadFile } from 'antd/es/upload/interface';
+
+import { UploadMovie } from '.';
 
 
 describe('The UploadMovie component', () => {
     let wrapper: ShallowWrapper<UploadMovie>;
+    const dispatch = jest.fn();
 
     beforeEach(() => {
         wrapper = shallow(
-            <UploadMovie />
+            <UploadMovie
+              dispatch={dispatch}
+            />
         );
     });
 
@@ -60,4 +64,50 @@ describe('The UploadMovie component', () => {
 
         expect(wrapper.state('title')).toEqual('poney');
     });
+
+    it('should update description state when changing description', () => {
+        const instance = wrapper.instance() as UploadMovie;
+        const event = {
+            target: {
+                value: 'poney',
+            },
+        } as unknown;
+
+        instance.handleDescription(event as SyntheticEvent);
+
+        expect(wrapper.state('description')).toEqual('poney');
+    });
+
+    it('should update short description state when changing short description', () => {
+        const instance = wrapper.instance() as UploadMovie;
+        const event = {
+            target: {
+                value: 'poney',
+            },
+        } as unknown;
+
+        instance.handleShortDescription(event as SyntheticEvent);
+
+        expect(wrapper.state('shortDescription')).toEqual('poney');
+    });
+
+    it('should dispatch a uploadMovie action when submitting form', () => {
+        const instance = wrapper.instance() as UploadMovie;
+        const state = {
+            title: 'poney',
+            description: 'poney magique',
+            shortDescription: 'my little poney',
+            posterFile: { name: 'poster' } as UploadFile,
+            movieFile: { name: 'video'} as UploadFile,
+        };
+
+        instance.setState(state);
+
+        instance.handleSubmit({ preventDefault(): void {} } as FormEvent);
+
+        expect(dispatch).toHaveBeenCalledWith({
+            type: 'oui',
+            payload: { ...state },
+        });
+    })
 });
