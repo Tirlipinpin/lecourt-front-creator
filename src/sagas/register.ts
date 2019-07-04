af8c99c3e060d2b0590b33f659c346862820a8d6
Henri Gaudeaux <henri.gaudeaux@one2team.com>
2019-07-04 13:37:53 +0200

    :wrench: corrected register url

--- src/sagas/register.ts
+++ src/sagas/register.ts
@@ -6,7 +6,7 @@ import { REGISTER_USER, REGISTER_USER_SUCCEEDED, REGISTER_USER_FAILED } from '..
 
 function* registerUser(action: any): any {
     try {
-        const token = yield axios.post('tokens/create', {
+        const token = yield axios.post('https://sso.stg.lecourt.tv/users/auth/register', {
             auth: {
                 username: 'user',
                 password: 'password',
